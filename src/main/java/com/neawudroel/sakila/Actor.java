87b3195da68a2164f05fc102e71f118139f54d45
JDoawp <joel.lovgrennordell@gmail.com>
2020-03-24 21:08:24 +0100

    Actor Class

--- src/main/java/com/neawudroel/sakila/Actor.java
+++ src/main/java/com/neawudroel/sakila/Actor.java
@@ -1,4 +1,47 @@
 package com.neawudroel.sakila;
 
-public class Actor {
+import javax.persistence.Column;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+public class Actor implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @Column(name = "actor_id", nullable = false)
+    private int actor_id;
+
+    @Column(name = "first_name", nullable = false)
+    private String first_name;
+
+    @Column(name = "last_name", nullable = false)
+    private String last_name;
+
+    @Column(name = "last_update", nullable = false)
+    private Timestamp last_update;
+
+    public int getActor_id() {
+        return actor_id;
+    }
+
+    public void setActor_id(int actor_id) {
+        this.actor_id = actor_id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
 }
